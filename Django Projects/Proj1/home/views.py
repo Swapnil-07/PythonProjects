4ddc2f543c1c6fd9f4ef6637cabd7fb29f4b25d6
Swapnil-07 <5993157+Swapnil-07@users.noreply.github.com>
2020-06-06 17:29:04 +0530

    First Commit

---
+++ Django Projects/Proj1/home/views.py
@@ -0,0 +1,34 @@
+from django.shortcuts import render, HttpResponse
+from datetime import datetime
+from home.models import Contact
+from django.contrib import messages
+
+# Create your views here.
+def index(request):
+    context = {
+        'variable1' : 'This is Sent as Variable',
+        'variable2' : 'This is Swapnil in Front OF You'
+    }
+    return render(request, 'index.html', context)
+    # return HttpResponse('This is Home Page')
+
+def about(request):
+    return render(request, 'about.html')
+    # return HttpResponse('This is About Page')
+
+def services(request):
+    return render(request, 'services.html')
+    # return HttpResponse('This is Services Page')
+
+def contact(request):
+    if request.method == "POST":
+        name = request.POST.get('name')
+        email = request.POST.get('email')
+        bio = request.POST.get('bio')
+
+        contact = Contact(name=name, email=email, bio=bio, date=datetime.today())
+        contact.save()
+        messages.success(request, 'Your Message has been Sent')
+
+    return render(request, 'contact.html')
+    # return HttpResponse('This is Contact Us Page')
