505ea64c24b3d62bfea26b426a102b39ecc32019
Swapnil-07 <5993157+Swapnil-07@users.noreply.github.com>
2020-06-11 17:58:01 +0530

    update 11-6-2020

--- Django Projects/UserProject/home/urls.py
+++ Django Projects/UserProject/home/urls.py
@@ -4,6 +4,6 @@ from home import views
 
 urlpatterns = [
     path('', views.index, name="home",),
-    path('login/', views.login, name="login"),
-    path('logout/', views.logout, name="logout")
+    path('login/', views.loginUser, name="login"),
+    # path('logout/', views.logoutUser, name="logout")
 ]
--- Django Projects/UserProject/home/views.py
+++ Django Projects/UserProject/home/views.py
@@ -1,11 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect 
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, logout
 
 # Create your views here.
 def index(request):
+    if request.user.is_anonymous:
+        return redirect('/login/')
     return render(request,'index.htm')
 
-def login(request):
+def loginUser(request):
+    if request.method == "POST":
+
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        # Check if user has entered correct Credentials
+        user = authenticate(username=username, password=password)
+        if user is not None:
+            return redirect("/")
+        else:
+            return render(request, 'login.htm')
+    
     return render(request,'login.htm')
 
-def logout(request):
-    return render(request,'logout.htm')
+def logoutUser(request):
+    logout(request)
+    return redirect('/login/')
